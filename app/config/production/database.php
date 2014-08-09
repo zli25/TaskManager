35b7fe494129ce667d0b48ae3854acfdd1ba7da5
Zibo Li <zibo.li25@gmail.com>
2014-08-09 11:33:14 -0400

    modified Boxfile and add database file for production

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'TaskManager',
+            'username'  => 'fiona',
+            'password'  => 'jEbKO35H',
+        ),
+
+    ),
+
+);
